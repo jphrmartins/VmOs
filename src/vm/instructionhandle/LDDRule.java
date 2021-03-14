bdc863e8ad15616d81f28f03dab7d9fb69dfdf50
vitor hugo <vitor_hgbds@hotmail.com>
2021-03-14 18:29:56 -0300

    lddRule

--- src/vm/instructionhandle/LDDRule.java
+++ src/vm/instructionhandle/LDDRule.java
@@ -5,6 +5,17 @@ import vm.InstructionRule;
 import vm.Opcode;
 import vm.Word;
 
-public class LDDRule {
+public class LDDRule implements InstructionRule {
+
+    @Override
+    public boolean shouldExecute(Opcode opcode) {
+        return opcode == Opcode.LDD;
+    }
+
+    @Override
+    public void executeRule(CPU cpu, Word instruction) {
+        cpu.getReg()[instruction.getR1()] = cpu.getMemory()[instruction.getP()].getP();
+        cpu.incrementPc();
+    }
     
 }
