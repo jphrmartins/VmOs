9d1f2b97966055e83e67a9f2635e0964cd9aa188
Vitor Hugo <vitor_hgbds@hotmail.com>
2021-04-28 01:09:13 -0300

    atualizando progMinimo

--- src/vm/programs/ProgMinimo.java
+++ src/vm/programs/ProgMinimo.java
@@ -1,10 +1,10 @@
 package vm.programs;
 
-import vm.Opcode;
-import vm.Word;
-
 import static vm.CPU.Register.ANY;
 import static vm.CPU.Register.R1;
+
+import vm.Opcode;
+import vm.Word;
 
 public class ProgMinimo implements Program {
 
@@ -17,7 +17,14 @@ public class ProgMinimo implements Program {
                 new Word(Opcode.STD, R1, ANY, 12),
                 new Word(Opcode.STD, R1, ANY, 13),
                 new Word(Opcode.STD, R1, ANY, 14),
-                new Word(Opcode.STOP, ANY, ANY, -1)
+                new Word(Opcode.STOP, ANY, ANY, -1),
+                Word.emptyWord(),
+                Word.emptyWord(),
+                Word.emptyWord(),
+                Word.emptyWord(),
+                Word.emptyWord(),
+                Word.emptyWord(),
+                Word.emptyWord()
         };
     }
 }
